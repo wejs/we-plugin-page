917c5765ac982c54fe960feb1dce109822009c69
albertosouza <alberto.souza.99@gmail.com>
2019-03-18 20:34:23 -0300

    fix content cron table name

--- cron.js
+++ cron.js
@@ -2,7 +2,7 @@ module.exports = function (we, done) {
   Promise.all([
     // Article publication:
     we.db.defaultConnection.query(
-      'UPDATE `content` SET `published`="1" WHERE publishedAt <= NOW() AND published != 1'
+      'UPDATE `contents` SET `published`="1" WHERE publishedAt <= NOW() AND published != 1'
     )
   ])
   .then( ()=> {
